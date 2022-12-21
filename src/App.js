0ffc01bcd8071f6c2a2adeb3af5c44a342ec2510
NeinShibes <76112694+NeinShibes@users.noreply.github.com>
2022-12-21 13:23:39 +0900

    add a Like button with set states

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ function App() {
   const blogTitle = "ReactBlog";
   let [titles, setTitles] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
   let [dates, setDates] = useState(['2월 17일 발행']);
+  let [likes, setLikes] = useState(0);
+
   return (
     <div className="App">
     <div className="black-nav">
       <div>{blogTitle}</div>
     </div>
+    <button onClick={()=>{setTitles(['여자 코트 추천', '강남 우동 맛집', '파이썬 독학'])}}>Change Title</button>
     <div className="list">
-      <h4>{titles[0]}</h4>
+      <h4>{titles[0]} <span onClick={()=>{setLikes(likes+1)}}>👍</span> {likes} </h4>
       <p>{dates[0]}</p>
     </div>
     <div className="list">
