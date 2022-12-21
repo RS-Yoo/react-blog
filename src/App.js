b3fdb3e6f398d1b8220351dd8f726adca5cba424
NeinShibes <76112694+NeinShibes@users.noreply.github.com>
2022-12-21 12:49:01 +0900

    add post titles with states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const blogTitle = "ReactBlog";
+  let [titles, setTitles] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [dates, setDates] = useState(['2월 17일 발행']);
   return (
     <div className="App">
     <div className="black-nav">
-      <div>Blog</div>
+      <div>{blogTitle}</div>
+    </div>
+    <div className="list">
+      <h4>{titles[0]}</h4>
+      <p>{dates[0]}</p>
+    </div>
+    <div className="list">
+      <h4>{titles[1]}</h4>
+      <p>{dates[0]}</p>
+    </div>
+    <div className="list">
+      <h4>{titles[2]}</h4>
+      <p>{dates[0]}</p>
     </div>
     </div>
   );
